a843c661270609c0d4b6fdf8db567c32f5aa5032
Michelle Erica <michelle.lo@thoughtworks.com>
2019-09-08 21:20:58 +1000

    Add test for img url

--- src/component/card/Card.test.js
+++ src/component/card/Card.test.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Card from './Card';
+import { BASE_PATH } from '../../config';
 
 describe('Card', () => {
-  it('displays the name', () => {
-    const testName = 'example name';
-
-    const wrapper = shallow(<Card name={testName}/>);
-
-    expect(wrapper.find('.card-name').text()).toEqual(testName);
-  });
-
   it('displays the an image', () => {
     const wrapper = shallow(<Card />);
 
     expect(wrapper.find('img')).toHaveLength(1);
+  });
+
+  it('displays a specific image', () => {
+    const imageName = "test";
+    const expectedUrl = `${BASE_PATH}/${imageName}.jpg`;
+
+    const wrapper = shallow(<Card image={imageName} />);
+    
+    expect(wrapper.find('img').prop('src')).toEqual(expectedUrl);
+  });
+
+  it('displays the name', () => {
+    const testName = 'example name';
+
+    const wrapper = shallow(<Card name={testName} />);
+
+    expect(wrapper.find('.card-name').text()).toEqual(testName);
   });
 });
